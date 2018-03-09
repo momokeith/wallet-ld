35400a702eae956ce6f18c835b249145af0f76b9
Mohamed Keita <mohamed.keita@tsi-payment.com>
2018-03-09 17:12:45 +0100

    update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def handle(event, context):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": "yes"
+        "body": '{"id":1,"balance":123, "currency":"EUR"}'
     }
